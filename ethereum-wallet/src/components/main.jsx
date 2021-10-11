96d9a519e5c77faeb542db158900c2f3c7f31e25
dimitar.b <dimitar.b@cooltecho.com>
2021-10-11 10:06:03 +0300

    add dependencies

---
+++ ethereum-wallet/src/components/main.jsx
@@ -0,0 +1,14 @@
+
+
+
+const Main = () => {
+
+    return(
+        <div>
+            <h1>This is simple ethereum node. For Deloveloping Purposes only. Please do not use it with main ethereum net</h1>
+        </div>
+       
+    )
+}
+
+export default Main
